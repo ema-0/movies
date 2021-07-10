02e9f1b7d33acf93ebdfa4e4f7a70ef9235209e6
ema <ema-0>
2021-07-10 11:22:40 +0200

    removed requireents and test.sh

--- test.sh
+++
@@ -1,1 +0,0 @@
-python3 movie_classifier/main.py --title "The Year of the Yao" --description "The Year of the Yao is a documentary film telling the first year of basketball player Yao Ming in the United States."
